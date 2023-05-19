912e5264418e823aa7b6138971454279180612b3
MustafaK17 <mustafakarademir2004@gmail.com>
2023-05-19 11:15:44 +0300

    100'den büyük sayılar
    100'den büyük sayıların 1-10 arasında bir sayıya rastgele bölünmesi için fonksiyon eklendi

--- pythonProject/odev.py
+++ pythonProject/odev.py
@@ -1,8 +1,15 @@
+import random
+
 while True:
     sayi = int(input("Bir sayı girin: "))
 
-    if sayi > 10:
+    if  100 > sayi > 10:
         sonuc=sayi**2
+
+    elif sayi >= 100:
+        bolen=random.randint(1,10)
+        sonuc=sayi/bolen
+        print("Bölen:", bolen)
 
     elif sayi<10 and sayi!=0:
         sonuc=sayi**3
