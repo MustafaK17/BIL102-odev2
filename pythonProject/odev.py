6174e35f79cd130ce82bf441cf91bb8490c39100
MustafaK17 <mustafakarademir2004@gmail.com>
2023-05-19 11:25:27 +0300

    hatalı giriş
    tam sayı dışında bir girdi olursa hata vermesi sağlandı

--- pythonProject/odev.py
+++ pythonProject/odev.py
@@ -1,30 +1,34 @@
 import random
 
 while True:
-    sayi = int(input("Bir sayı girin: "))
+    try:
+        giris = input("Lütfen bir sayı giriniz:")
+        sayi=int(giris)
 
-    if  100 > sayi > 10:
-        sonuc=sayi**2
+        if  100 > sayi > 10:
+            sonuc=sayi**2
 
-    elif 1000 >= sayi >= 100:
-        bolen=random.randint(1,10)
-        sonuc=sayi/bolen
-        print("Bölen:", bolen)
+        elif 1000 >= sayi >= 100:
+            bolen=random.randint(1,10)
+            sonuc=sayi/bolen
+            print("Bölen:", bolen)
 
-    elif sayi < 0:
-        sayi=-sayi
-        sonuc=sayi
+        elif sayi < 0:
+            sayi=-sayi
+            sonuc=sayi
 
-    elif sayi > 1000:
-        bolen=random.randint(50,250)
-        sonuc=sayi/bolen
-        print("Bölen:", bolen)
+        elif sayi > 1000:
+            bolen=random.randint(50,250)
+            sonuc=sayi/bolen
+            print("Bölen:", bolen)
 
-    elif sayi<10 and sayi!=0:
-        sonuc=sayi**3
+        elif sayi<10 and sayi!=0:
+            sonuc=sayi**3
 
-    elif sayi==0:
-        quit()
+        elif sayi==0:
+            quit()
 
-    print("Sonuç:", sonuc)
+        print("Sonuç:", sonuc)
 
+    except ValueError:
+        print("Hatalı giriş yaptınız, lütfen bir sayı giriniz!")
